b507d33d69ffb7c7078928e501985f3f09cf5ebb
Jannik Arndt <jannik@jannikarndt.de>
2015-07-06 14:54:38 +0200

    Zwischenstand 2: Create-Methode eingefügt

--- MyBank/Controllers/HomeController.cs
+++ MyBank/Controllers/HomeController.cs
@@ -25,5 +25,24 @@ namespace MyBank.Controllers
             var accounts = _bank.Accounts.Where(account => account.OwnerId == userId).ToList();
             return View(accounts);
         }
+
+        [Authorize]
+        public RedirectToRouteResult Create()
+        {
+            // Falls es schon Konten gibt, finde die höchste Kontonummer von allen und addiere 1, sonst nimm 1:
+            var accountNumber = _bank.Accounts.Any() ? _bank.Accounts.Max(account => account.Number) + 1 : 1;
+
+            // Erstelle ein neues Konto mit der aktuellen UserId als Besitzer und der eben herausgefundenen Kontonummer:
+            var newAccount = new BankAccount(User.Identity.GetUserId(), accountNumber);
+
+            // Füge das Konto der Liste der Konten der Bank hinzu:
+            _bank.Accounts.Add(newAccount);
+
+            // Speichere die Änderungen in die Datenbank:
+            _bank.SaveChanges();
+
+            // Zeige die Übersichtsseite (Index) an:
+            return RedirectToAction("Index");
+        }
     }
 }
